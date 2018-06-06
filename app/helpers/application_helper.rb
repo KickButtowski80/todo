91c6e04cf8e4d4b1a9075fdbe2adf159989b1367
kickbuttowski59 <pazpaz22@yahoo.com>
2018-06-06 19:32:24 +0000

    labled completed task with table-dark

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,15 @@
 module ApplicationHelper
     
     def completed?(list)
-        if list.status == 'low'
+        if list.completed           
+            'table-dark'
+        elsif list.status == 'low'
             'table-secondary'
         elsif list.status == 'medium'
             'table-primary'
         elsif list.status == 'high'
             'table-warning'
-        else
+        elsif list.status == 'urgent'
             'table-danger'
         end        
     end
